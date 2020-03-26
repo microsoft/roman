b8d07af480a8aee781f1cff6bddddad25846639b
drMJ <mihaijal@microsoft.com>
2020-03-26 15:51:01 -0700

    Fixed urdf file references to work on linux

--- src/sim/sim.py
+++ src/sim/sim.py
@@ -1,8 +1,10 @@
 import numpy as np
 import pybullet as p
 import time
+import os
 p.connect(p.GUI)
-p.loadURDF("c:\\code\\manipulation\\sim\\roman.urdf")
+urdfFile = os.path.join(os.path.dirname(__file__), 'roman.urdf')
+p.loadURDF(urdfFile)
 
 p.setGravity(0,0,-10)
 p.setJointMotorControl2(0, 1, controlMode=p.VELOCITY_CONTROL, force=10)
