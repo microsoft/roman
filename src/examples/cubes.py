f8b973f682925bbc5dce9355faa75ce575105fb9
drMJ <mihaijal@microsoft.com>
2021-02-07 14:45:51 -0800

    cube stacking example

--- src/examples/cubes.py
+++ src/examples/cubes.py
@@ -8,18 +8,18 @@ import math
 import time
 
 cubePoses = [[-0.15,-0.3,0.025], [-0.35,-0.1,0.025], [-0.55,-0.3,0.025]]
-
+cubeSize =0.05
 def setup_sim(simenv):
-    simenv.make_box([0.05]*3, cubePoses[0], color=(0.8,0.2,0.2,1), mass = 0.1)
-    simenv.make_box([0.05]*3, cubePoses[1], color=(0.2,0.8,0.2,1), mass = 0.1)
-    simenv.make_box([0.05]*3, cubePoses[2], color=(0.2,0.2,0.8,1), mass = 0.1)
+    simenv.make_box([cubeSize]*3, cubePoses[0], color=(0.8,0.2,0.2,1), mass = 0.1)
+    simenv.make_box([cubeSize]*3, cubePoses[1], color=(0.2,0.8,0.2,1), mass = 0.1)
+    simenv.make_box([cubeSize]*3, cubePoses[2], color=(0.2,0.2,0.8,1), mass = 0.1)
 
 if __name__ == '__main__':
     # start simulation
     robot = connect(use_sim=True, sim_init=setup_sim)
     home_pose = robot.arm.state.tool_pose().clone()
     stack_pose = home_pose.clone()
-    stack_pose[2] = -0.1 # under the table
+    grasp_depth = 0.01
     robot.hand.move(128)
     robot.hand.set_mode(hand.GraspMode.PINCH)
     
@@ -29,12 +29,13 @@ if __name__ == '__main__':
         pose[:3] = cubePoses[i]
         pose[2] = 0.1 # pre-grasp
         robot.arm.move(pose, max_speed=2, max_acc=1)
-        pose[2] = 0.04
+        pose[2] = cubeSize - grasp_depth
         robot.arm.move(pose)
         robot.hand.close()
         pose[2] = 0.1 # pre-grasp
         robot.arm.move(pose)
-        robot.arm.move(home_pose, max_speed=2, max_acc=1)
+        robot.arm.move(home_pose, max_speed=1, max_acc=0.2)
+        stack_pose[2] = (i+1) * cubeSize - grasp_depth
         robot.arm.touch(stack_pose)
         robot.hand.move(128)
         robot.arm.move(home_pose, max_speed=2, max_acc=1)
