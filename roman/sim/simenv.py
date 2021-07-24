c47052fbd43a165a792f30e34deb1dfd1a68033a
drMJ <mihaijal@microsoft.com>
2021-07-24 16:11:03 -0700

    helper functions for creating objects in sim

--- roman/sim/simenv.py
+++ roman/sim/simenv.py
@@ -9,12 +9,12 @@ from . import rq
 ## configures the simulated environment
 ################################################################
 class SimEnv:
-    TIME_STEP = 1/240.
+    TIME_STEP = 1 / 240.
 
     '''
     Loads the default pybullet environment and allows further configuration of the cameras, objects present in the scene etc.
     '''
-    def __init__(self, useGUI = True):
+    def __init__(self, useGUI=True):
         self._useGUI = useGUI
         self.__time = 0.0
 
@@ -29,7 +29,7 @@ class SimEnv:
 
     def reset(self):
         pb.resetSimulation()
-        pb.setGravity(0,0,-10)
+        pb.setGravity(0, 0, -10)
 
     def update(self):
         pb.stepSimulation()
@@ -41,16 +41,42 @@ class SimEnv:
     def disconnect(self):
         pb.disconnect()
 
-    def loadURDF(self, urdf, basePosition=[0,0,0], baseOrientation = [0,0,0]):
-        return pb.loadURDF(urdf, basePosition = basePosition, baseOrientation=pb.getQuaternionFromEuler(baseOrientation), flags = pb.URDF_USE_SELF_COLLISION | pb.URDF_USE_SELF_COLLISION_EXCLUDE_ALL_PARENTS )
+    def loadURDF(self, urdf, basePosition=[0, 0, 0], baseOrientation=[0, 0, 0]):
+        return pb.loadURDF(urdf, basePosition=basePosition, baseOrientation=pb.getQuaternionFromEuler(baseOrientation), flags=pb.URDF_USE_SELF_COLLISION | pb.URDF_USE_SELF_COLLISION_EXCLUDE_ALL_PARENTS )
 
-    def make_box(self, size, position=[0,0,0], orientation=[0,0,0,1], mass=0, tex=None, color=None):
-        cid = pb.createCollisionShape(pb.GEOM_BOX, halfExtents = np.array(size)*0.5)
-        vid = pb.createVisualShape(pb.GEOM_BOX, halfExtents = np.array(size)*0.5)
-        id = pb.createMultiBody(mass,baseCollisionShapeIndex=cid, baseVisualShapeIndex=vid, basePosition=position, baseOrientation=orientation)
+    def make_box(self, size, position=[0, 0, 0], orientation=[0, 0, 0, 1], mass=0, tex=None, color=None, restitution=1):
+        cid = pb.createCollisionShape(pb.GEOM_BOX, halfExtents=np.array(size) * 0.5)
+        vid = pb.createVisualShape(pb.GEOM_BOX, halfExtents=np.array(size) * 0.5)
+        id = pb.createMultiBody(mass, baseCollisionShapeIndex=cid, baseVisualShapeIndex=vid, basePosition=position, baseOrientation=orientation)
         if tex is not None:
             pb.changeVisualShape(id, -1, textureUniqueId=tex)
         if color is not None:
             pb.changeVisualShape(id, -1, rgbaColor=color)
-        #pb.changeDynamics(id, -1, restitution = 0.5)
+        pb.changeDynamics(id, -1, restitution=restitution)
         return id
+
+    def make_ball(self, radius, position=[0, 0, 0], mass=0, tex=None, color=None, restitution=1):
+        cid = pb.createCollisionShape(pb.GEOM_SPHERE, radius=radius)
+        vid = pb.createVisualShape(pb.GEOM_SPHERE, radius=radius)
+        id = pb.createMultiBody(mass, baseCollisionShapeIndex=cid, baseVisualShapeIndex=vid, basePosition=position)
+        if tex is not None:
+            pb.changeVisualShape(id, -1, textureUniqueId=tex)
+        if color is not None:
+            pb.changeVisualShape(id, -1, rgbaColor=color)
+        pb.changeDynamics(id, -1, restitution=restitution)
+        return id
+
+    def load_obj(self, mesh_file, position, orientation, scale, mass, vhacd_file=None, tex=None, color=None):
+        '''Like loadURDF, but simpler and without requiring a urdf file. Supports concave objects (requires a vhacd file).
+        Use pb.vhacd(in_mesh_file, out_vhacd_file, log_file, alpha=0.04,resolution=50000 ) to generate a vhacd file. '''
+
+        vhacd_file = vhacd_file if vhacd_file else mesh_file
+        cid = pb.createCollisionShape(pb.GEOM_MESH, fileName=vhacd_file, meshScale=scale, collisionFrameOrientation=orientation)
+        vid = pb.createVisualShape(pb.GEOM_MESH, fileName=mesh_file, meshScale=scale, visualFrameOrientation=orientation)
+        id = pb.createMultiBody(mass, baseCollisionShapeIndex=cid, baseVisualShapeIndex=vid, basePosition=position)
+        if tex is not None:
+            pb.changeVisualShape(id, -1, textureUniqueId=tex)
+        if color is not None:
+            pb.changeVisualShape(id, -1, rgbaColor=color)
+        return id
+
